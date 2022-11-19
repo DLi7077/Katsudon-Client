56eb1339764b21dac34b7718399110d580d1e3ad
Devin Li <51217000+DLi7077@users.noreply.github.com>
2022-11-18 23:10:05 -0500

    Automate configs for development

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,12 +2,11 @@ import axios from "axios";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const PROD_API_TARGET = "https://katsudon-server-v2.herokuapp.com/";
+
 export default async function Api(props) {
   const { method, endpoint, params, headers, body } = props;
-
-  const TARGET_URL = process.env.REACT_APP_DEVELOPMENT
-    ? process.env.REACT_APP_DEV_TARGET
-    : process.env.REACT_APP_API_TARGET;
+  const TARGET_URL = process.env.REACT_APP_DEV_TARGET ?? PROD_API_TARGET;
 
   const request = {
     method: method,
