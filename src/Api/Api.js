cee454789be694748f5d3c60a55687506a4f3865
butterriolu <derpvin12@gmail.com>
2022-11-18 23:00:53 -0500

    fix config

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -5,9 +5,10 @@ dotenv.config();
 export default async function Api(props) {
   const { method, endpoint, params, headers, body } = props;
 
-  const TARGET_URL = process.env.REACT_APP_DEVELOPMENT
-    ? process.env.REACT_APP_DEV_TARGET
-    : process.env.REACT_APP_API_TARGET;
+  const TARGET_URL =
+    process.env.REACT_APP_DEVELOPMENT === "true"
+      ? process.env.REACT_APP_DEV_TARGET
+      : process.env.REACT_APP_API_TARGET;
 
   const request = {
     method: method,
