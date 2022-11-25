e3918924b120b822e4ef635d114f4d8fbc90226c
butterriolu <derpvin12@gmail.com>
2022-11-24 21:23:32 -0500

    replaced userTools with redux userSlice

--- src/Constants/navbar.js
+++ src/Constants/navbar.js
@@ -1,10 +1,8 @@
-import currentUser from "../Utils/UserTools";
-
 export const MENU_LINKS = {
   Welcome: "/welcome",
   Users: "/users",
   Progress: "/weekly-progress",
-  Profile: `/profile?user_id=${currentUser("user-id")}`,
+  Profile: "/profile",
   Register: "/register",
   Login: "/login",
 };
