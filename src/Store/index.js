31908bcb54ef246f943964635c42092c982f8a0c
butterriolu <derpvin12@gmail.com>
2022-11-27 02:28:39 -0500

    added color to load bar

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./Reducers/user";
 import snackbarReducer from "./Reducers/snackbar";
 import progressReducer from "./Reducers/progress";
+import themeReducer from "./Reducers/theme";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     snackbar: snackbarReducer,
-    progress : progressReducer
+    progress: progressReducer,
+    theme: themeReducer,
   },
 });
