f1b3ddd0bbd29062a3ba8c45213cbd34e9a05896
butterriolu <derpvin12@gmail.com>
2022-12-03 17:47:52 -0500

    remove unused leaderboard page

--- src/Views/Leaderboard/index.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-import { get, map } from "lodash";
-import { Button } from "@mui/material";
-import CodeBlock from "../../Components/Codeblock";
-
-export default function Leaderboard() {
-  
-}
