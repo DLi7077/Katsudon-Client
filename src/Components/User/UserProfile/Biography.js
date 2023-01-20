8a50da4233d4aaadcef197d468663c82a7080ae4
butterriolu <derpvin12@gmail.com>
2023-01-19 20:08:14 -0500

    separate avatar upload and biography edit from profile component

---
+++ src/Components/User/UserProfile/Biography.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { get } from "lodash";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import EditIcon from "@mui/icons-material/Edit";
+import { IconButton, TextField } from "@mui/material";
+import UserAPI from "../../../Api/UserAPI";
+import { updateProfileBiography } from "../../../Store/Reducers/user";
+import {
+  setSnackbarSuccess,
+  setSnackbarWarning,
+} from "../../../Store/Reducers/snackbar";
+
+export default function Biography(props) {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user);
+  const [editingBio, setEditingBio] = useState(false);
+  const [updatedBio, setUpdatedBio] = useState(currentUser.biography);
+  function startEditBiography() {
+    setEditingBio(true);
+  }
+
+  function cancelUpdatedBiography() {
+    setEditingBio(false);
+  }
+
+  async function submitUpdatedBiography() {
+    if (currentUser.biography === updatedBio) {
+      setEditingBio(false);
+      return;
+    }
+
+    await UserAPI.updateBiography(updatedBio)
+      .then((res) => {
+        const updatedBiography = get(res.user, "biography");
+        dispatch(updateProfileBiography({ biography: updatedBiography }));
+        return updatedBiography;
+      })
+      .then((updatedBiography) => {
+        setUpdatedBio(updatedBiography);
+        dispatch(setSnackbarSuccess("Updated biography"));
+      })
+      .catch(() => {
+        dispatch(setSnackbarWarning("Couldn't update biography"));
+      })
+      .finally(() => {
+        setEditingBio(false);
+      });
+  }
+
+  return (
+    <div className="profile-biography" style={{ position: "relative" }}>
+      {!editingBio &&
+        (get(currentUser, "user_id") === props.userInfo._id
+          ? currentUser.biography
+          : props.userInfo.biography ?? "")}
+
+      {get(currentUser, "user_id") === props.userInfo._id && !editingBio && (
+        <IconButton
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            color: "white",
+            padding: "0.25rem",
+          }}
+          onClick={() => {
+            startEditBiography();
+          }}
+        >
+          <EditIcon />
+        </IconButton>
+      )}
+      {editingBio && (
+        <>
+          <TextField
+            variant="standard"
+            value={updatedBio}
+            multiline
+            rows={4}
+            color="primary"
+            inputProps={{
+              style: {
+                fontSize: "1.35rem",
+                color: "white",
+                textAlign: "center",
+              },
+            }}
+            onChange={(e) => {
+              setUpdatedBio(e.target.value);
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+            }}
+          >
+            <IconButton
+              style={{ color: "#FFAC7D", padding: "0.25rem" }}
+              onClick={cancelUpdatedBiography}
+            >
+              <CloseIcon />
+            </IconButton>
+            <IconButton
+              style={{ color: "#7AFF87", padding: "0.25rem" }}
+              onClick={submitUpdatedBiography}
+            >
+              <CheckIcon />
+            </IconButton>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/Components/User/UserProfile/ProfileAvatar.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./styles.css";
+import { Avatar } from "@mui/material";
+import UserAPI from "../../../Api/UserAPI";
+import { useDispatch, useSelector } from "react-redux";
+import { updateProfilePicture } from "../../../Store/Reducers/user";
+import {
+  setSnackbarSuccess,
+  setSnackbarWarning,
+} from "../../../Store/Reducers/snackbar";
+import { get } from "lodash";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+
+export default function ProfileAvatar(props) {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user);
+
+  async function handleUploadProfilePicture(event) {
+    if (!event.target.files) return;
+
+    const bannerPicture = event.target.files[0];
+    const formData = new FormData();
+    formData.append("imgfile", bannerPicture);
+
+    await UserAPI.uploadProfilePicture(formData, get(currentUser, "auth_token"))
+      .then((res) => {
+        dispatch(
+          updateProfilePicture({
+            profile_picture_url: `${
+              res.user.profile_picture_url
+            }?${global.Date.now()}`, // force rerender
+          })
+        );
+
+        dispatch(setSnackbarSuccess("Uploaded profile picture"));
+      })
+      .catch(() => {
+        dispatch(setSnackbarWarning("File size must be < 5MB"));
+      });
+  }
+
+  function ProfileUpload() {
+    return (
+      <>
+        <input
+          type="file"
+          name="imgfile"
+          accept="image/*"
+          id="upload-profile-picture"
+          onChange={handleUploadProfilePicture}
+          hidden
+        />
+        <label htmlFor="upload-profile-picture">
+          <div
+            className={
+              get(currentUser, "logged_in") &&
+              get(currentUser, "user_id") === props.userInfo._id
+                ? "profile-picture-upload"
+                : ""
+            }
+          >
+            <UploadFileIcon style={{ fontSize: "2.5rem" }} />
+            Upload Profile Picture
+          </div>
+        </label>
+      </>
+    );
+  }
+  return (
+    <div className="profile-picture" style={{ position: "relative" }}>
+      <Avatar
+        src={
+          get(currentUser, "user_id") === props.userInfo._id
+            ? currentUser.profile_picture_url
+            : get(props, "userInfo.profile_picture_url")
+        }
+        style={{
+          width: "100px",
+          height: "100px",
+          border: "2px solid white",
+          ...props.style,
+        }}
+      />
+      {get(currentUser, "user_id") === props.userInfo._id && <ProfileUpload />}
+    </div>
+  );
+}
